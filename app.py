9bcd7655fcfd853e3e76e390372ea12c8def7623
soieu <soieu9898@gmail.com>
2024-10-22 16:47:54 +0900

    ajrkekffkwuTsl?

--- app.py
+++ app.py
@@ -5,15 +5,18 @@ import os
 
 app = FastAPI()
 
+
 @app.get("/ping")
 def ping():
     global first_run_flag
     status_code = 201 if first_run_flag else 200
     return Response("pong", status_code=status_code)
 
+
 @app.get("/health")
 async def health():
     return Response("pong")
+
 
 if __name__ == "__main__":
     uvicorn.run("app:app", host="0.0.0.0", port=8080)
